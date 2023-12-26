8e8cf771317d21b59db3fe7db4af5914b1d39af7
VRomanenko04 <vovasan2004@gmail.com>
2023-12-26 20:38:46 +0200

    Залил фотографии, сделал массив объектов проектов

--- src/projects.ts
+++ src/projects.ts
@@ -1,2 +1,38 @@
+import project1Img from './assets/project_main_img_1.png';
+import project2Img from './assets/project_main_img_2.png';
+import project3Img from './assets/project_main_img_3.png';
+import project4Img from './assets/project_main_img_4.png';
+
+
 export const types = ['Show all', 'Typography', '2D & 3D', 'Animations', 'UI/UX', 'Personal Projects', 'Projects for clients', 'Other'];
 
+export const projects = [
+    {
+        title: 'Logo',
+        types: ['Show all', '2D & 3D', 'Projects for clients'],
+        shortDescription: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore, earum accusamus officiis modi perferendis delectus voluptatibus libero, esse omnis voluptas tempore nemo at ipsam suscipit!',
+        modalDescription: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus perferendis, consectetur debitis tempore odio quidem fuga quasi officia nostrum molestiae sequi ipsum? Labore, quas commodi voluptatum quam rerum facere natus ipsum eveniet quis illum odio nobis mollitia ipsam officia libero amet cumque et deserunt? Mollitia.',
+        image: {project1Img}, 
+    },
+    {
+        title: 'Fonts',
+        types: ['Show all', 'Typography', 'Personal Projects'],
+        shortDescription: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore, earum accusamus officiis modi perferendis delectus voluptatibus libero, esse omnis voluptas tempore nemo at ipsam suscipit!',
+        modalDescription: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus perferendis, consectetur debitis tempore odio quidem fuga quasi officia nostrum molestiae sequi ipsum? Labore, quas commodi voluptatum quam rerum facere natus ipsum eveniet quis illum odio nobis mollitia ipsam officia libero amet cumque et deserunt? Mollitia.',
+        image: {project2Img}, 
+    },
+    {
+        title: '3D animation',
+        types: ['Show all', '2D & 3D', 'Personal Projects', 'Animations'],
+        shortDescription: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore, earum accusamus officiis modi perferendis delectus voluptatibus libero, esse omnis voluptas tempore nemo at ipsam suscipit!',
+        modalDescription: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus perferendis, consectetur debitis tempore odio quidem fuga quasi officia nostrum molestiae sequi ipsum? Labore, quas commodi voluptatum quam rerum facere natus ipsum eveniet quis illum odio nobis mollitia ipsam officia libero amet cumque et deserunt? Mollitia.',
+        image: {project3Img}, 
+    },
+    {
+        title: 'SpaceDev',
+        types: ['Show all', 'UI/UX', '2D & 3D', 'Projects for clients'],
+        shortDescription: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore, earum accusamus officiis modi perferendis delectus voluptatibus libero, esse omnis voluptas tempore nemo at ipsam suscipit!',
+        modalDescription: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus perferendis, consectetur debitis tempore odio quidem fuga quasi officia nostrum molestiae sequi ipsum? Labore, quas commodi voluptatum quam rerum facere natus ipsum eveniet quis illum odio nobis mollitia ipsam officia libero amet cumque et deserunt? Mollitia.',
+        image: {project4Img}, 
+    },
+]
