75155955f019a45c13f970c1fae259f37637e9b6
0-0nmuk <nazarmukovnin132@gmail.com>
2024-02-10 01:11:06 +0000

    изменил описание в info block

--- src/components/InfoBlock/InfoBlock.tsx
+++ src/components/InfoBlock/InfoBlock.tsx
@@ -15,7 +15,7 @@ const InfoBlock = () => {
                     <span className={styles.small__span}>el</span>
                     <span className={styles.big__span}>lo</span>
                 </h1>
-                <h3 className={styles.info__text}>I`m Nazar - <span>Graphic & Motion designer</span> & sometimes illustrator/animator and a blah blah blah and a bit 3d artist and a great guy</h3>
+                <h3 className={styles.info__text}>I`m Nazar - <span>Graphic & Motion designer</span> & sometimes illustrator/animator a bit 3d artist and a great guy</h3>
                 <button onClick={() => setActive(true)} className={styles.contacts}>My Contacts</button>
                 <p className={styles.infoblock__footer}>here you can see some of my <span className={styles.underline_span}>projects</span> <span>;)</span></p>
             </div>
